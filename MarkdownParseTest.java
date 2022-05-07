0426e3455ae5eb9a2bedfeda846064a258b6ddae
Yiwwwwww <tragedyfigure@gmail.com>
2022-05-07 12:48:11 -0700

    Commit

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,24 +1,34 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
 public class MarkdownParseTest {
 
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }
-    @Test
-    public void test(){
-        MarkdownParse.getLinks(markdown);
-
-
-
-
+    public void test() throws IOException{
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList("https://something.com","some-page.html"));
+	    assertEquals(expected, links);
+        
     }
 
+    public void test2() throws IOException{
+        Path fileName = Path.of("test-file-2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList("https://something.com","some-page.html"));
+	    assertEquals(expected, links);
+    }
 
   
   
   
   
     }
-}
