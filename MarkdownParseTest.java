ba8a4f30a916e78ed90b28ec8f69362a8182ea1f
Yiwwwwww <tragedyfigure@gmail.com>
2022-05-08 16:13:49 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -11,15 +11,18 @@ public class MarkdownParseTest {
 
     @Test
     public void test() throws IOException{
+        System.out.println("Test1");
         Path fileName = Path.of("test-file.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         ArrayList<String> expected = new ArrayList<>(Arrays.asList("https://something.com","some-page.html"));
 	    assertEquals(expected, links);
         
+        
     }
 
     public void test2() throws IOException{
+        System.out.println("Test2");
         Path fileName = Path.of("test-file-2.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
