2cabf571370e1b52e2e50b382a23e9c057337367
Yiwwwwww <tragedyfigure@gmail.com>
2022-05-27 08:50:26 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -10,37 +10,17 @@ import java.nio.file.Path;
 public class MarkdownParseTest {
 
     @Test
-    public void test() throws IOException{
-        System.out.println("Test1");
-        Path fileName = Path.of("test-file.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(content);
-        ArrayList<String> expected = new ArrayList<>(Arrays.asList("https://something.com","some-page.html"));
-	    assertEquals(expected, links);
-        
-        
-    }
-    @Test
-    public void test2() throws IOException{
-        System.out.println("Test2");
-        Path fileName = Path.of("test-file-2.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(content);
-        ArrayList<String> expected = new ArrayList<>(Arrays.asList("https://something.com","some-page.html"));
-	    assertEquals(expected, links);
-    }
-    @Test
     public void testSnippet1() throws IOException{
-        System.out.println("Test3");
+        
         Path fileName = Path.of("Snippet1.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
-        ArrayList<String> expected = new ArrayList<>(Arrays.asList("url.com","google.com","ucsd.edu"));
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList("`google.com","google.com","ucsd.edu"));
 	    assertEquals(expected, links);
     }
     @Test
     public void testSnippet2() throws IOException{
-        System.out.println("Test4");
+        
         Path fileName = Path.of("Snippet2.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
@@ -49,11 +29,11 @@ public class MarkdownParseTest {
     }
     @Test
     public void testSnippet3() throws IOException{
-        System.out.println("Test5");
+        
         Path fileName = Path.of("Snippet3.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
-        ArrayList<String> expected = new ArrayList<>(Arrays.asList("https://www.twitter.com","https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule"));
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule"));
 	    assertEquals(expected, links);
     }
   
